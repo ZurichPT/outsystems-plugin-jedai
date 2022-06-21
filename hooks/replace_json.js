3d83886a4abb3f1cd5c21ffa510c748c1859db1d
ZurichPT <107210450+ZurichPT@users.noreply.github.com>
2022-06-21 16:48:43 +0100

    Update replace_json.js

--- hooks/replace_json.js
+++ hooks/replace_json.js
@@ -4,7 +4,7 @@ module.exports = function(context) {
 
     var anagogJSON = path.join(context.opts.projectRoot, 'platforms/android/app/src/main/assets/anagog_config.json');
     
-      var newJSON = '{"userDefinedStats":{"Name":"STRING","Age":"INTEGER","Score":"DECIMAL"},"notificationChannelId":"CordovaAppChannel","foregroundChannelId":"","enableAudienceAnalytics":true,"foregroundMode":"NEVER","enableLatestMs":true,"enableCrashReports":true,"enableInternalLog":true}';
+      var newJSON = '{"userDefinedStats":{"cms_MissingData": "STRING","cms_UnmatchingData": "STRING","cms_FrozenData": "STRING","cms_AnagogConsent": "STRING"},"notificationChannelId":"CordovaAppChannel","foregroundChannelId":"","enableAudienceAnalytics":true,"foregroundMode":"NEVER","enableLatestMs":true,"enableCrashReports":true,"enableInternalLog":true}';
       fs.writeFile(anagogJSON, newJSON, 'utf8', function (err) {
         if (err) 
           {throw new Error('>>> Unable to write into anagog_config.json: ' + err);}
